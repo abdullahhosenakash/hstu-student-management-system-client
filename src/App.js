0785c6246cbf78b9dd8c002a14fb90e520bda262
abdullahhosenakash <abdullahhosenakash@gmail.com>
2022-06-16 23:38:25 +0600

    code added

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/home' element={<Home />} />
-        <Route path='/login' element={<Login />} />
+        <Route path='/Login' element={<Login />} />
 
         <Route path='*' element={<NotFound />} />
       </Routes>
