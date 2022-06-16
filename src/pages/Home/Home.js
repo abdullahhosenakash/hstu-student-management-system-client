d96ba7bf697d4b1435ec52d8bbb66e473fe60f99
abdullahhosenakash <abdullahhosenakash@gmail.com>
2022-06-16 22:01:37 +0600

    header added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from '../shared/Header';
 
 const Home = () => {
     return (
         <div>
-            Welcome To HSTU Enrollment System
+            <Header />
         </div>
     );
 };
