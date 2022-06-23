db71763e682894a022efb52e0e3b331975046ffd
abdullahhosenakash <abdullahhosenakash@gmail.com>
2022-06-23 15:59:29 +0600

    admin profile updated

--- src/pages/AdminPanel/AdminProfile.js
+++ src/pages/AdminPanel/AdminProfile.js
@@ -1,28 +1,31 @@
 import React from 'react';
 import { FloatingLabel, Form } from 'react-bootstrap';
+import useToken from '../../hooks/useToken';
 
 const AdminProfile = () => {
+    const { loggedInUser } = useToken();
+    const { userEmail, userName, phone } = loggedInUser;
     return (
         <div className='update-profile mx-auto mb-5'>
             <FloatingLabel
                 label="Name"
                 className="mb-3"
             >
-                <Form.Control value={''} disabled />
+                <Form.Control value={userName} disabled />
             </FloatingLabel>
 
             <FloatingLabel
                 label="Email address"
                 className="mb-3"
             >
-                <Form.Control value={'userEmail'} disabled />
+                <Form.Control value={userEmail} disabled />
             </FloatingLabel>
 
             <FloatingLabel
                 label="Phone"
-                className="mb-3 w-50 ps-1 d-inline-block"
+                className="mb-3"
             >
-                <Form.Control value={'phone'} disabled />
+                <Form.Control value={phone} disabled />
             </FloatingLabel>
 
         </div>
